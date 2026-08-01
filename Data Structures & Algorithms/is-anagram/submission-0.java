2a22f1a89564e971d99a9ad9100158af0e78d705
MOHANRAJ S <mohanrajvbl123@gmail.com>
2026-08-01 11:28:35 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) return false;
+        int[] arr=new int[26];
+
+        for(char ch : s.toCharArray()){
+            arr[ch - 'a']++;
+        }
+
+        for(char ch : t.toCharArray()){
+            arr[ch- 'a']--;
+        }
+
+        Arrays.sort(arr);
+
+        for(int  i =0 ;i<arr.length;i++){
+            if(arr[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
